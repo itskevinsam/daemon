f335db1fb1070ee2ce3c37c706c02103a99d4041
itskevinsam <itskevinsam@yahoo.co.in>
2016-08-10 15:22:36 +0530

    GreetUser.php
    Add Greet Test

---
+++ GreetUser.php
@@ -0,0 +1,12 @@
+<?php
+
+class GreetUser {
+  private $name;
+  public function __construct($name) {
+    $this->name = $name;
+  }
+  public function __toString() {
+   return $this->name; 
+  }
+}
+
